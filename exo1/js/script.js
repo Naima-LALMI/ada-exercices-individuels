b26531f48098a8930be28f94da1edb104490bef7
Naima <naima.lalmi28@gmail.com>
2024-02-09 18:40:42 +0100

    ajout exo 1

---
+++ exo1/js/script.js
@@ -0,0 +1,14 @@
+
+
+let surname = prompt("👋 Bonjour, " + "quel est ton nom ? ")
+let msg= "👋 Bonjour " +  surname
+console.log(msg)
+document.body.innerHTML +=  "<h2>" +  msg + "</h2>"
+
+let anneeNaissance = prompt("Quel est ton année de naissance ?")
+let today = new Date()
+let year = today.getFullYear()
+let age = year - anneeNaissance
+console.log(age)
+document.body.innerHTML += "<h3>" + " Vous avez "+ age + " ans" + "</h3>"
+
