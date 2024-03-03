7315582d687cfb668aea8c89df02fff099b2715b
Naima <naima.lalmi28@gmail.com>
2024-03-03 21:32:08 +0100

    debut etape 3 + correction noté

--- exo2/index.js
+++ exo2/index.js
@@ -11,20 +11,70 @@ function askNumber() {
 function didIWin(number) {
   if (number < 22) {
     alert("Plus grand");
+    return false;
   } else if (number > 22) {
     alert("Plus petit");
+    return false;
   } else {
     alert("Bravo ! Vous avez deviné le nombre");
+    return true;
   }
 }
 
 //didIWin();
 // console.log(result);
 
+//etape 3
 function gamePlay() {
-  let givenNumber = askNumber();
-  didIWin(givenNumber);
+  let result = false;
+  if (result == false) {
+    let givenNumber = askNumber();
+    result = didIWin(givenNumber);
+  }
 }
 gamePlay();
 
-//Appel de la fct avec le résultat de Étape 1 directement
+// CORRECTION PAR KRIS
+
+// function askNumber() {
+//   let givenNumber;
+//   givenNumber = parseInt(prompt("Give me a number"));
+
+//   return givenNumber;
+// }
+
+// function askNumberToGuess() {
+//   let numberToGuess = -1;
+//   while (numberToGuess < 0 || numberToGuess > 50) {
+//     numberToGuess = parseInt(
+//       prompt("Give me a number to guess between 0 and 50")
+//     );
+//   }
+
+//   return numberToGuess;
+// }
+
+// function didIWin(number, numberToGuess) {
+//   if (number < numberToGuess) {
+//     alert("Plus grand");
+//     return false;
+//   } else if (number > numberToGuess) {
+//     alert("Plus petit");
+//     return false;
+//   } else {
+//     alert("Bravo ! Vous avez deviné le nombre");
+//     return true;
+//   }
+// }
+
+// function gamePlay() {
+//   let numberToGuess = askNumberToGuess();
+
+//   let result = false;
+//   while (result == false) {
+//     let givenNumber = askNumber();
+//     result = didIWin(givenNumber, numberToGuess);
+//   }
+// }
+
+// gamePlay();
